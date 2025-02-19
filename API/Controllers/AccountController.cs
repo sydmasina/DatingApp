2d33a54a42f8bb9ba09c4543f3552821bd8952f5
sydmasina <masinasydwell@gmail.com>
2025-02-19 17:26:12 +0200

    Add register account endpoint

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace API.Controllers
+{
+    public class AccountController(DataContext context) : BaseApiController
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+            try
+            {
+                bool isUsernameTaken = await IsUsernameTaken(registerDto.Username);
+
+                if (isUsernameTaken)
+                {
+                    return BadRequest("Username already exists.");
+                }
+
+                using var hmac = new HMACSHA512();
+
+                var user = new AppUser
+                {
+                    UserName = registerDto.Username.ToLower(),
+                    PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                    PasswordSalt = hmac.Key
+                };
+
+                context.Users.Add(user);
+                await context.SaveChangesAsync();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Unable to add user. \nError: {ex.Message} \n {ex.StackTrace}");
+            }
+        }
+
+        private async Task<bool> IsUsernameTaken(string username)
+        {
+            return await context.Users.AnyAsync(user => user.UserName.ToLower() == username.ToLower());
+        }
+    }
+}
