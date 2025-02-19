bc6daf47b343b431cd41828f3677bdefe7e0cb89
sydmasina <masinasydwell@gmail.com>
2025-02-19 11:22:18 +0200

    Setup api call to fetch data

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { User } from './models/user';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  http = inject(HttpClient)
   title = 'DatingApp';
+  users: User[] = [];
+  isLoading: boolean = true;
+
+  async ngOnInit() {
+    await this.http.get('https://localhost:7159/api/Users').subscribe({
+      next: response => this.users = response as User[],
+      error: error=> console.log(error),
+      complete: ()=>{ this.isLoading = false; }
+    })
+  }
 }
