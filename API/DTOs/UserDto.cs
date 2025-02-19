fd9f1004df354bfba5502815494f483830a83b5d
sydmasina <masinasydwell@gmail.com>
2025-02-19 21:12:18 +0200

    Add UserDto for login

---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class UserDto
+    {
+        [Required]
+        public required string Username { get; set; }
+
+        [Required]
+        public required string Token { get; set; }
+    }
+}
