af87f081c92c1edfc82c8094fbba7ebe1a1362e4
sydmasina <masinasydwell@gmail.com>
2025-02-20 14:07:50 +0200

    implement loading spinner

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,25 +2,29 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { User } from './shared/models/user';
 import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [CommonModule]
+  imports: [CommonModule, MatProgressSpinnerModule],
 })
-export class AppComponent implements OnInit{
-  http = inject(HttpClient)
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
   title = 'DatingApp';
   users: User[] = [];
   isLoading: boolean = true;
 
   async ngOnInit() {
     await this.http.get('https://localhost:7159/api/Users').subscribe({
-      next: response => this.users = response as User[],
-      error: error=> console.log(error),
-      complete: ()=>{ this.isLoading = false; console.log("Request has completed")}
-    })
+      next: (response) => (this.users = response as User[]),
+      error: (error) => console.log(error),
+      complete: () => {
+        this.isLoading = false;
+        console.log('Request has completed');
+      },
+    });
   }
 }
