faf35ecaa07d340eee69210def2fada6f06f1702
sydmasina <masinasydwell@gmail.com>
2025-02-20 13:58:20 +0200

    Add shared folder

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
-import { User } from './models/user';
+import { User } from './shared/models/user';
 import { CommonModule } from '@angular/common';
 
 @Component({
