1b5fa4821d2952b8a6d2d53636fd39973f3251dd
sydmasina <masinasydwell@gmail.com>
2025-02-20 11:17:39 +0200

    refactor services to use extensions

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,24 @@
+﻿using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, 
+            IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+
+            services.AddOpenApi();
+            services.AddSwaggerGen();
+            services.AddDbContext<DataContext>(options => options.UseSqlite(connectionString));
+            services.AddCors();
+            services.AddScoped<ITokenService, TokenService>();
+
+            return services;
+        }
+    }
+}
