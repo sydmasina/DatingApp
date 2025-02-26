84e27f0458a7ee25077596b760a6790386451927
sydmasina <masinasydwell@gmail.com>
2025-02-26 18:40:16 +0200

    Update nav, and add dropdown

--- client/src/app/shared/components/nav/nav.component.ts
+++ client/src/app/shared/components/nav/nav.component.ts
@@ -2,27 +2,42 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Login } from '../../models/login';
 import { AccountService } from '../../services/account.service';
+import { CommonModule } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule, BsDropdownModule],
   templateUrl: './nav.component.html'
 })
 export class NavComponent {
   constructor(private accountService: AccountService) {}
+  isLoggedIn: boolean = false;
   loginModel: Login = {
     username: '',
     password: ''
   };
 
-  async login(){
+  login(){
     if(this.loginModel.username === '' || this.loginModel.password === ''){
       console.log('Username and password are required');
       return;
     }
 
-    var response = await this.accountService.login(this.loginModel);
-    console.log(response);
+    this.accountService.login(this.loginModel).subscribe({
+      next: (response) => {
+        console.log(response);
+        if(response.token === null || response.token === ''){
+          throw new Error("Login failed");
+        }
+        this.isLoggedIn = true;
+      },
+      error: error => console.log(error)
+    });
+  }
+
+  logout(){
+    this.isLoggedIn = false;
   }
 }
