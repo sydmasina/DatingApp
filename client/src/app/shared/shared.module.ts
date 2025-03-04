dcf11a656b63838cf75b21e82a74c664de5902f7
sydmasina <masinasydwell@gmail.com>
2025-03-04 20:52:09 +0200

    add home component

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, NavComponent],
-  exports: [NavComponent],
+  imports: [CommonModule, NavComponent, HomeComponent],
+  exports: [NavComponent, HomeComponent],
 })
 export class SharedModule {}
