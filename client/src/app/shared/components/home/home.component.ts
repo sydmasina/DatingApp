6c1d240ff10e7423fe246c1b11ec962d5ed2aa6d
sydmasina <masinasydwell@gmail.com>
2025-03-05 18:34:23 +0200

    Update home page

--- client/src/app/shared/components/home/home.component.ts
+++ client/src/app/shared/components/home/home.component.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  registerMode: boolean = false;
 
+  registerToggle(){
+    this.registerMode = !this.registerMode;
+  }
 }
