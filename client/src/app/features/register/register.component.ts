9b892158c4cde78a51cc30f642923607b4075506
sydmasina <masinasydwell@gmail.com>
2025-03-11 06:36:26 +0200

    Update register page

--- client/src/app/features/register/register.component.ts
+++ client/src/app/features/register/register.component.ts
@@ -1,25 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, input, output} from '@angular/core';
 import { Register } from '../../shared/models/register';
 import { FormsModule } from '@angular/forms';
+import { User } from '../../shared/models/user';
+import { CommonModule } from '@angular/common';
+import { AccountService } from '../../shared/services/account.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  cancelRegister = output<boolean>();
   registerModel: Register = {
     username: '',
     password: ''
   };
 
+  constructor(private accountService: AccountService){}
+
   register() {
-    console.log('Registering...', this.registerModel);
+    this.accountService.register(this.registerModel).subscribe({
+      next: response => {
+        console.log(response);
+        this.cancel();
+      },
+      error: error => console.log(error)
+    })
   }
 
   cancel() {
-    console.log('cancelled');
+    this.cancelRegister.emit(false);
   }
 }
