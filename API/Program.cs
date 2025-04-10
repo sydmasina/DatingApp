7b53d3e02d8fb47f925f63638ad13446ee82f661
sydmasina <masinasydwell@gmail.com>
2025-04-10 05:50:54 +0200

    Add Exception Middleware handler

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,8 @@ builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 // Enable CORS
 app.UseCors(x => x.AllowAnyHeader()
