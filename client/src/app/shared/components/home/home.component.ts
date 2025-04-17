59f5dd48fc3f3e624b108a6915f51149db836f70
sydmasina <masinasydwell@gmail.com>
2025-04-17 15:16:52 +0200

    Clean up

--- client/src/app/shared/components/home/home.component.ts
+++ client/src/app/shared/components/home/home.component.ts
@@ -1,8 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { RegisterComponent } from '../../../features/register/register.component';
-import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -11,14 +10,8 @@ import { UserService } from '../../services/user.service';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   registerMode: boolean = false;
-
-  constructor(private _userService: UserService) {}
-
-  ngOnInit(): void {
-    this._userService.getUsers();
-  }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
@@ -26,9 +19,5 @@ export class HomeComponent implements OnInit {
 
   onCancelRegister(event: boolean) {
     this.registerMode = event;
-  }
-
-  get isLoading() {
-    return this._userService.isRunningFetchData;
   }
 }
