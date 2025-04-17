2b6916442c2e90ee3fab8f7cc11d4b463fd0c854
sydmasina <masinasydwell@gmail.com>
2025-04-17 12:05:05 +0200

    Update user type

--- client/src/app/shared/models/user.ts
+++ client/src/app/shared/models/user.ts
@@ -1,4 +1,22 @@
 export interface User {
-    id: number;
-    userName: string;
+  id: number;
+  userName: string;
+  age: string;
+  knownAs: string;
+  photoUrl: string;
+  created: string;
+  lastActive: string;
+  gender: string;
+  introduction: string;
+  interests: string;
+  lookingFor: string;
+  city: string;
+  country: string;
+  photos: Photo[];
+}
+
+export interface Photo {
+  id: number;
+  url: string;
+  isMain: boolean;
 }
