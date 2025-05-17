68f51fa225b16f0c53f18ab05e684eae565da666
sydmasina <masinasydwell@gmail.com>
2025-05-17 16:14:26 +0200

    Add cloudinary service

---
+++ API/Services/CloudinaryService.cs
@@ -0,0 +1,18 @@
+﻿using API.Models;
+using CloudinaryDotNet;
+using Microsoft.Extensions.Options;
+
+namespace API.Services
+{
+    public class CloudinaryService
+    {
+        public Cloudinary Cloudinary { get; }
+
+        public CloudinaryService(IOptions<CloudinarySettings> config)
+        {
+            var settings = config.Value;
+            var account = new Account(settings.CloudName, settings.ApiKey, settings.ApiSecret);
+            Cloudinary = new Cloudinary(account);
+        }
+    }
+}
