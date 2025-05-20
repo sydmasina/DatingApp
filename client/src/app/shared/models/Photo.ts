438094da31dddf273a8739d3594e6c27ce43c30b
sydmasina <masinasydwell@gmail.com>
2025-05-20 13:40:12 +0200

    update photo model

--- client/src/app/shared/models/Photo.ts
+++ client/src/app/shared/models/Photo.ts
@@ -2,4 +2,10 @@ export interface Photo {
   id: number;
   url: string;
   isMain: boolean;
+  publicId: string;
+}
+
+export interface PhotoToDelete {
+  DbId: number;
+  PublicId: string | null;
 }
