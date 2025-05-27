feddb57b3ba8a3ce757e8efb6f7338f3a84a35ea
sydmasina <masinasydwell@gmail.com>
2025-05-27 13:26:49 +0200

    set required fields

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -14,13 +14,17 @@ namespace API.DTOs
         public string KnownAs { get; set; }
         [Required]
         public string Gender { get; set; }
+        [Required]
         public string? Introduction { get; set; }
+        [Required]
         public string? Interests { get; set; }
+        [Required]
         public string? LookingFor { get; set; }
         [Required]
         public string City { get; set; }
         [Required]
         public string Country { get; set; }
+        [Required]
         public List<PhotoToUpload>? ImagesToUpload { get; set; }
     }
 }
