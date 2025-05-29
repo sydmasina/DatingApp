01bc4d8ac75d43af19ec4ef33656a493ba55c9fe
sydmasina <masinasydwell@gmail.com>
2025-05-29 14:05:43 +0200

    Add gender and CurrentUsername to use as filter values

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -4,8 +4,11 @@
     {
         private const int MaxPageSize = 50;
         public int PageNumber { get; set; } = 1;
+
         private int _pageSize = 10;
 
+        public string? Gender { get; set; }
+        public string? CurrentUsername { get; set; }
         public int PageSize
         {
             get => _pageSize;
