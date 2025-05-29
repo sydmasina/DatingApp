1a2d3a7bdd5e01d0993dd58c8e59e2032e20d319
sydmasina <masinasydwell@gmail.com>
2025-05-29 21:09:18 +0200

    Add more api user filters: age range, country and city

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -9,6 +9,11 @@
 
         public string? Gender { get; set; }
         public string? CurrentUsername { get; set; }
+        public int? MinAge { get; set; }
+        public int? MaxAge { get; set; }
+        public string? Country { get; set; }
+        public string? City { get; set; }
+
         public int PageSize
         {
             get => _pageSize;
