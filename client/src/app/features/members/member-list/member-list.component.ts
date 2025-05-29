d819bc3fc81609929aebd66b85ef2c37d09e82f4
sydmasina <masinasydwell@gmail.com>
2025-05-29 06:32:17 +0200

    Add paginator to users list

--- client/src/app/features/members/member-list/member-list.component.ts
+++ client/src/app/features/members/member-list/member-list.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, effect } from '@angular/core';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { UserService } from '../../../shared/services/user.service';
 import { MemberCardComponent } from './member-card/member-card.component';
@@ -7,21 +8,51 @@ import { MemberCardComponent } from './member-card/member-card.component';
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [CommonModule, MatProgressSpinnerModule, MemberCardComponent],
+  imports: [
+    CommonModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MemberCardComponent,
+  ],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css',
 })
 export class MemberListComponent {
-  constructor(private _userService: UserService) {}
+  pageNumber = 1;
+  pageSize = 5;
+  pageSizeOptions: number[] = [5, 10, 25, 50];
+
+  constructor(private _userService: UserService) {
+    effect(() => {
+      const pagination = this._userService.paginatedUsers()?.pagination;
+      if (pagination) {
+        this.pageSize = pagination.itemsPerPage;
+      }
+    });
+  }
 
   ngOnInit(): void {
-    if (this._userService.users().length === 0) {
-      this._userService.fetchUsers();
+    if (!this._userService.paginatedUsers()) {
+      this._loadUsers();
     }
   }
 
+  private _loadUsers() {
+    this._userService.fetchUsers(this.pageNumber, this.pageSize);
+  }
+
+  handlePageEvent(e: PageEvent) {
+    this.pageSize = e.pageSize;
+    this.pageNumber = e.pageIndex + 1;
+    this._loadUsers();
+  }
+
   get users() {
-    return this._userService.users();
+    return this._userService.paginatedUsers()?.items;
+  }
+
+  get pagination() {
+    return this._userService.paginatedUsers()?.pagination;
   }
 
   get isLoading() {
