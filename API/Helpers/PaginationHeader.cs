721a3f0c10034bce6d63b9909626a16f39046a36
sydmasina <masinasydwell@gmail.com>
2025-06-02 17:52:49 +0200

    fix typo

--- API/Helpers/PaginationHeader.cs
+++ API/Helpers/PaginationHeader.cs
@@ -2,7 +2,7 @@
 {
     public class PaginationHeader(int currentPage, int itemsPerPage, int totalItems, int totalPages)
     {
-        public int CurrntPage { get; set; } = currentPage;
+        public int CurrentPage { get; set; } = currentPage;
         public int ItemsPerPage { get; set; } = itemsPerPage;
         public int TotalItems { get; set; } = totalItems;
         public int TotalPages { get; set; } = totalPages;
