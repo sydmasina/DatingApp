8045df321a6759ca542cd6fa3fba68d0d02af425
sydmasina <masinasydwell@gmail.com>
2025-06-05 16:24:52 +0200

    Set required fields and update migration

--- API/Data/Migrations/20250605142316_addMassagesTable.Designer.cs
+++ API/Data/Migrations/20250605142316_addMassagesTable.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20250605140255_addMassagesTable")]
+    [Migration("20250605142316_addMassagesTable")]
     partial class addMassagesTable
     {
         /// <inheritdoc />
--- API/Models/Message.cs
+++ API/Models/Message.cs
@@ -3,9 +3,9 @@
     public class Message
     {
         public int Id { get; set; }
-        public string SenderUsername { get; set; }
-        public string RecipientUsername { get; set; }
-        public string Content { get; set; }
+        public required string SenderUsername { get; set; }
+        public required string RecipientUsername { get; set; }
+        public required string Content { get; set; }
         public DateTime? DateRead { get; set; }
         public DateTime MessageSent { get; set; } = DateTime.UtcNow;
         public bool SenderDeleted { get; set; }
