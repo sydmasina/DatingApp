439cae6a22e10b9b175d91d632f35c59580f15b2
sydmasina <masinasydwell@gmail.com>
2025-06-05 10:18:29 +0200

    Add card tooltip

--- client/src/app/features/members/member-list/member-card/member-card.component.ts
+++ client/src/app/features/members/member-list/member-card/member-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, input } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterLink } from '@angular/router';
 import { User } from '../../../../shared/models/user';
 
 @Component({
   selector: 'app-member-card',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatTooltipModule],
   templateUrl: './member-card.component.html',
   styleUrl: './member-card.component.css',
 })
