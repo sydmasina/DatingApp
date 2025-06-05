994e292051b7d83b177b76c5cec6b25d22895c4d
sydmasina <masinasydwell@gmail.com>
2025-06-05 11:40:00 +0200

    Update member card to enable like toggle functionality

--- client/src/app/features/members/member-list/member-card/member-card.component.ts
+++ client/src/app/features/members/member-list/member-card/member-card.component.ts
@@ -1,7 +1,8 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterLink } from '@angular/router';
 import { User } from '../../../../shared/models/user';
+import { LikesService } from '../../../../shared/services/likes.service';
 
 @Component({
   selector: 'app-member-card',
@@ -12,4 +13,22 @@ import { User } from '../../../../shared/models/user';
 })
 export class MemberCardComponent {
   user = input.required<User>();
+  hasLiked = computed(() =>
+    this._likeService.likeIds().includes(this.user().id)
+  );
+  constructor(private _likeService: LikesService) {}
+
+  handleLikeToggle() {
+    this._likeService.toggleLike(this.user().id).subscribe({
+      next: () => {
+        if (this.hasLiked()) {
+          this._likeService.likeIds.update((ids) =>
+            ids.filter((x) => x !== this.user().id)
+          );
+        } else {
+          this._likeService.likeIds.update((ids) => [...ids, this.user().id]);
+        }
+      },
+    });
+  }
 }
