c0907e6e2953435aa2572b8b6bce3360f2a043a1
sydmasina <masinasydwell@gmail.com>
2025-06-06 05:34:27 +0200

    Like by refactor

--- client/src/app/features/matches/liked-by-list/liked-by-list.component.ts
+++ client/src/app/features/matches/liked-by-list/liked-by-list.component.ts
@@ -1,23 +1,38 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { PredicateTypes } from '../../../shared/constants/pagination';
+import { PaginationParams } from '../../../shared/models/user-params';
 import { LikesService } from '../../../shared/services/likes.service';
 import { MemberCardComponent } from '../../members/member-list/member-card/member-card.component';
 
 @Component({
   selector: 'app-liked-by-list',
   standalone: true,
-  imports: [CommonModule, MemberCardComponent],
+  imports: [CommonModule, MemberCardComponent, MatPaginatorModule],
   templateUrl: './liked-by-list.component.html',
   styleUrl: './liked-by-list.component.css',
 })
 export class LikedByListComponent implements OnInit {
+  paginationParams: PaginationParams = { pageNumber: 1, pageSize: 5 };
+  pageSizeOptions: number[] = [5, 10, 25, 50];
   constructor(private _likesService: LikesService) {}
 
   ngOnInit(): void {
-    this._likesService.getLikedBy();
+    this._likesService.getLikes(PredicateTypes.likedBy, this.paginationParams);
+  }
+
+  handlePageEvent(e: PageEvent) {
+    this.paginationParams.pageSize = e.pageSize;
+    this.paginationParams.pageNumber = e.pageIndex + 1;
+    this._likesService.getLikes(PredicateTypes.likedBy, this.paginationParams);
   }
 
   get LikedBy() {
-    return this._likesService.likedBy();
+    return this._likesService.likedBy()?.items;
+  }
+
+  get totalItems() {
+    return this._likesService.likedBy()?.pagination?.totalItems;
   }
 }
