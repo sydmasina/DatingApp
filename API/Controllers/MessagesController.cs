de6e16d1e0cabbc46a1b2572592c6329765c3ad7
sydmasina <masinasydwell@gmail.com>
2025-06-06 07:10:58 +0200

    Setup API message controller endpoints and repositories

---
+++ API/Controllers/MessagesController.cs
@@ -0,0 +1,64 @@
+﻿using API.DTOs;
+using API.Extensions;
+using API.Helpers;
+using API.Interfaces;
+using API.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class MessagesController(IMessageRepository messageRepository,
+        IUserRepository<AppUser> userRepository, IMapper mapper) : BaseApiController
+    {
+        [HttpPost]
+        public async Task<ActionResult<MessageDto>> CreatedMessage(CreateMessageDto createMessageDto)
+        {
+            var username = User.GetUserName();
+
+            if (username == createMessageDto.RecipientUsername.ToLower())
+                return BadRequest("You cannot message yourself");
+
+            var sender = await userRepository.GetUserByUsernameAsync(username);
+            var recipient = await userRepository.GetUserByUsernameAsync(createMessageDto.RecipientUsername);
+
+            if (recipient == null || sender == null) return BadRequest("Error sending message");
+
+            var message = new Message
+            {
+                Sender = sender,
+                Recipient = recipient,
+                SenderUsername = username,
+                RecipientUsername = createMessageDto.RecipientUsername,
+                Content = createMessageDto.Content
+            };
+
+            messageRepository.AddMessage(message);
+
+            if (await messageRepository.SaveAllAsync()) return Ok(mapper.Map<MessageDto>(message));
+
+            return BadRequest("Failed to save message");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessagesForUser(
+            [FromQuery] MessageParams messageParams)
+        {
+            messageParams.Username = User.GetUserName();
+
+            var messages = await messageRepository.GetMessagesForUser(messageParams);
+
+            Response.AddPaginationHeader(messages);
+
+            return messages;
+        }
+
+        [HttpGet("thread/{username}")]
+        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessageThread(string username)
+        {
+            var currentUsername = User.GetUserName();
+
+            return Ok(await messageRepository.GetMessageThread(currentUsername, username));
+        }
+    }
+}
