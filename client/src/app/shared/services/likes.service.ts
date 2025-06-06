40daa1323881cd20991c74936bab3c14107346db
sydmasina <masinasydwell@gmail.com>
2025-06-06 05:33:19 +0200

    Refactor likes service

--- client/src/app/shared/services/likes.service.ts
+++ client/src/app/shared/services/likes.service.ts
@@ -1,34 +1,33 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
 import { Injectable, signal, Signal } from '@angular/core';
 import { LikesEndpoint } from '../constants/api-enpoints/likes';
+import { PredicateTypes } from '../constants/pagination';
+import { PaginatedResult } from '../models/pagination';
 import { User } from '../models/user';
+import { PaginationParams } from '../models/user-params';
+import { setPaginatedResult, setPaginationParams } from '../utils/helpers';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LikesService {
-  private readonly _matches = signal<User[]>([]);
-  public readonly matches: Signal<User[]> = this._matches.asReadonly();
+  private readonly _matches = signal<PaginatedResult<User[]> | null>(null);
+  public readonly matches: Signal<PaginatedResult<User[]> | null> =
+    this._matches.asReadonly();
 
-  private readonly _likedBy = signal<User[]>([]);
-  public readonly likedBy: Signal<User[]> = this._likedBy.asReadonly();
+  private readonly _likedBy = signal<PaginatedResult<User[]> | null>(null);
+  public readonly likedBy: Signal<PaginatedResult<User[]> | null> =
+    this._likedBy.asReadonly();
 
   public readonly likeIds = signal<number[]>([]);
 
-  private readonly _likedByMe = signal<User[]>([]);
-  public readonly likedByMe: Signal<User[]> = this._likedByMe.asReadonly();
+  private readonly _likedByMe = signal<PaginatedResult<User[]> | null>(null);
+  public readonly likedByMe: Signal<PaginatedResult<User[]> | null> =
+    this._likedByMe.asReadonly();
 
-  private readonly _isFetchingMatches = signal<boolean>(false);
-  public readonly isFetchingMatches: Signal<boolean> =
-    this._isFetchingMatches.asReadonly();
-
-  private readonly _isFetchingLikedBy = signal<boolean>(false);
-  public readonly isFetchingLikedBy: Signal<boolean> =
-    this._isFetchingLikedBy.asReadonly();
-
-  private readonly _isFetchingLikedByMe = signal<boolean>(false);
-  public readonly isFetchingLikedByMe: Signal<boolean> =
-    this._isFetchingLikedBy.asReadonly();
+  private readonly _isFetchingLikes = signal<boolean>(false);
+  public readonly isFetchingLikes: Signal<boolean> =
+    this._isFetchingLikes.asReadonly();
 
   constructor(private _httpClient: HttpClient) {}
 
@@ -44,54 +43,36 @@ export class LikesService {
     });
   }
 
-  getMatches() {
-    if (this.isFetchingMatches()) {
-      return;
-    }
-
+  getLikes(predicate: PredicateTypes, paginationParams: PaginationParams) {
     let params = new HttpParams();
-    params = params.append('predicate', 'matches');
+    params = params.append('predicate', predicate);
+    params = setPaginationParams(params, paginationParams);
 
-    this._httpClient.get<User[]>(LikesEndpoint, { params }).subscribe({
-      next: (response) => {
-        this._matches.set(response);
-        this._isFetchingMatches.set(false);
-      },
-      error: () => this._isFetchingMatches.set(false),
-    });
+    this._httpClient
+      .get<User[]>(LikesEndpoint, {
+        observe: 'response',
+        params,
+      })
+      .subscribe({
+        next: (response) => {
+          this.updateLikes(response, predicate);
+          this._isFetchingLikes.set(false);
+        },
+        error: () => this._isFetchingLikes.set(false),
+      });
   }
 
-  getLikedBy() {
-    if (this.isFetchingLikedBy()) {
-      return;
+  updateLikes(response: HttpResponse<User[]>, predicate: PredicateTypes) {
+    switch (predicate) {
+      case PredicateTypes.liked:
+        setPaginatedResult(response, this._likedByMe);
+        break;
+      case PredicateTypes.likedBy:
+        setPaginatedResult(response, this._likedBy);
+        break;
+      default:
+        setPaginatedResult(response, this._matches);
+        break;
     }
-
-    let params = new HttpParams();
-    params = params.append('predicate', 'likedBy');
-
-    this._httpClient.get<User[]>(LikesEndpoint, { params }).subscribe({
-      next: (response) => {
-        this._likedBy.set(response);
-        this._isFetchingLikedBy.set(false);
-      },
-      error: () => this._isFetchingLikedBy.set(false),
-    });
-  }
-
-  getLikedByMe() {
-    if (this.isFetchingLikedByMe()) {
-      return;
-    }
-
-    let params = new HttpParams();
-    params = params.append('predicate', 'liked');
-
-    this._httpClient.get<User[]>(LikesEndpoint, { params }).subscribe({
-      next: (response) => {
-        this._likedByMe.set(response);
-        this._isFetchingLikedByMe.set(false);
-      },
-      error: () => this._isFetchingLikedByMe.set(false),
-    });
   }
 }
