3d896d15ebac3b8012ffa9c956d99053a2cb0e26
sydmasina <masinasydwell@gmail.com>
2025-06-11 05:56:00 +0200

    Refactor message list and add side drawer for message thread

--- client/src/app/features/messages/messages.component.ts
+++ client/src/app/features/messages/messages.component.ts
@@ -1,32 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
-import { RouterLink } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
-import { MessageContainerType } from '../../../shared/constants/message';
-import { Message } from '../../../shared/models/message';
-import { PaginationParams } from '../../../shared/models/user-params';
-import { MessageService } from '../../../shared/services/message.service';
-import { TimeAgoPipe } from '../../../shared/utils/time-ago.pipe';
+import { MessageContainerType } from '../../shared/constants/message';
+import { Message } from '../../shared/models/message';
+import { PaginationParams } from '../../shared/models/user-params';
+import { MessageService } from '../../shared/services/message.service';
+import { TimeAgoPipe } from '../../shared/utils/time-ago.pipe';
+import { MessageThreadComponent } from './message-thread/message-thread.component';
 
 @Component({
-  selector: 'app-message-list',
+  selector: 'app-messages',
   standalone: true,
   imports: [
     MatPaginatorModule,
     ButtonsModule,
     FormsModule,
     TimeAgoPipe,
-    RouterLink,
+    MatSidenavModule,
+    MatButtonModule,
+    MessageThreadComponent,
   ],
-  templateUrl: './message-list.component.html',
-  styleUrl: './message-list.component.css',
+  templateUrl: './messages.component.html',
+  styleUrl: './messages.component.css',
 })
-export class MessageListComponent implements OnInit {
+export class MessagesComponent implements OnInit {
   paginationParams: PaginationParams = { pageNumber: 1, pageSize: 5 };
   pageSizeOptions: number[] = [5, 10, 25, 50];
   container: MessageContainerType = MessageContainerType.Inbox;
   messageContainerType = MessageContainerType;
+  messageThreadUsername: string | null = null;
 
   constructor(private _messageService: MessageService) {}
 
@@ -48,6 +53,10 @@ export class MessageListComponent implements OnInit {
     if (this.container === MessageContainerType.Outbox)
       return `/messages/${message.recipientUsername}`;
     else return `/messages/${message.senderUsername}`;
+  }
+
+  sentMessageThreadUsername(username: string) {
+    this.messageThreadUsername = username;
   }
 
   get messages() {
