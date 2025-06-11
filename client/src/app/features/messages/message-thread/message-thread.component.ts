12fa0d79ac2450ba8c04420dbc89d74d12e16b3c
sydmasina <masinasydwell@gmail.com>
2025-06-11 17:19:36 +0200

    Add send message functionality

--- client/src/app/features/messages/message-thread/message-thread.component.ts
+++ client/src/app/features/messages/message-thread/message-thread.component.ts
@@ -1,20 +1,23 @@
 import { TitleCasePipe } from '@angular/common';
 import { Component, input, OnInit, signal } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Message } from '../../../shared/models/message';
+import { Message, SendMessageBody } from '../../../shared/models/message';
 import { AuthService } from '../../../shared/services/auth.service';
 import { MessageService } from '../../../shared/services/message.service';
 
 @Component({
   selector: 'app-message-thread',
   standalone: true,
-  imports: [TitleCasePipe],
+  imports: [TitleCasePipe, FormsModule, ReactiveFormsModule],
   templateUrl: './message-thread.component.html',
   styleUrl: './message-thread.component.css',
 })
 export class MessageThreadComponent implements OnInit {
   public readonly messageThread = signal<Message[]>([]);
   username = input.required<string>();
+  userPhotoUrl = input.required<string>();
+  newMessageControl = new FormControl('');
 
   constructor(
     private _messageService: MessageService,
@@ -34,8 +37,30 @@ export class MessageThreadComponent implements OnInit {
     });
   }
 
-  onOpenProfile(username: string) {
-    this._router.navigate(['./members/' + username]);
+  onOpenProfile() {
+    this._router.navigate(['./members/' + this.username()]);
+  }
+
+  sendMessage() {
+    if (this.newMessageControl.value === '') {
+      return;
+    }
+
+    const messagePayload: SendMessageBody = {
+      recipientUsername: this.username(),
+      content: this.newMessageControl.value ?? '',
+    };
+
+    this._messageService.sendMessage(messagePayload).subscribe({
+      next: (response) => {
+        this.messageThread.update((messageThread) => [
+          ...messageThread,
+          response,
+        ]);
+      },
+    });
+
+    this.newMessageControl.setValue('');
   }
 
   get currentUser() {
