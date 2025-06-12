f5715f12e8e31fd4a4e53e5b400237eb9fb6115b
sydmasina <masinasydwell@gmail.com>
2025-06-12 14:12:30 +0200

    Remove messageThread and redirect to memberDetails

--- client/src/app/features/messages/messages.component.ts
+++ client/src/app/features/messages/messages.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
-import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
-import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { MessageContainerType } from '../../shared/constants/message';
 import { Message } from '../../shared/models/message';
 import { PaginationParams } from '../../shared/models/user-params';
 import { MessageService } from '../../shared/services/message.service';
 import { TimeAgoPipe } from '../../shared/utils/time-ago.pipe';
-import { MessageThreadComponent } from './message-thread/message-thread.component';
 
 @Component({
   selector: 'app-messages',
@@ -20,24 +18,20 @@ import { MessageThreadComponent } from './message-thread/message-thread.componen
     ButtonsModule,
     FormsModule,
     TimeAgoPipe,
-    MatSidenavModule,
     MatButtonModule,
-    MessageThreadComponent,
   ],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css',
 })
 export class MessagesComponent implements OnInit {
-  @ViewChild('drawer') drawer!: MatDrawer;
   paginationParams: PaginationParams = { pageNumber: 1, pageSize: 5 };
   pageSizeOptions: number[] = [5, 10, 25, 50];
   container: MessageContainerType = MessageContainerType.Inbox;
   messageContainerType = MessageContainerType;
-  messageThreadUsername: string | null = null;
 
   constructor(
     private _messageService: MessageService,
-    private route: ActivatedRoute
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -46,17 +40,6 @@ export class MessagesComponent implements OnInit {
 
   loadMessages() {
     this._messageService.getMessages(this.paginationParams, this.container);
-
-    const username = this.route.snapshot.paramMap.get('username');
-    if (this.drawer && username) {
-      this.messageThreadUsername = username;
-      this.drawer.open();
-      return;
-    }
-
-    if (this.drawer && this.drawer.opened) {
-      this.drawer.close();
-    }
   }
 
   handlePageEvent(e: PageEvent) {
@@ -71,15 +54,15 @@ export class MessagesComponent implements OnInit {
     else return `/messages/${message.senderUsername}`;
   }
 
-  sentMessageThreadUsername(message: Message) {
-    this.messageThreadUsername =
+  redirectToMessageThread(message: Message) {
+    const messageThreadUsername =
       this.container === MessageContainerType.Outbox
         ? message.recipientUsername
         : message.senderUsername;
-  }
 
-  handleDrawerClosed() {
-    this.messageThreadUsername = null;
+    this.router.navigate([
+      '/members/' + messageThreadUsername + '/open-messages',
+    ]);
   }
 
   get messages() {
